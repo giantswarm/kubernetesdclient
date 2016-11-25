65b7c2c4aeaf899217f58997e9647d07bd5fde3f
Tim Schindler <xh3b4sd@gmail.com>
2016-11-25 15:34:05 +0100

    extended client with services

--- client.go
+++ client.go
@@ -2,12 +2,22 @@
 // Kubernetesd API.
 package kubernetesdclient
 
-import "github.com/go-resty/resty"
+import (
+	"net/url"
+
+	"github.com/go-resty/resty"
+
+	"github.com/giantswarm/kubernetesdclient/service/creator"
+	"github.com/giantswarm/kubernetesdclient/service/root"
+)
 
 // Config represents the configuration used to create a new client object.
 type Config struct {
 	// Dependencies.
 	RestClient *resty.Client
+
+	// Settings.
+	Address string
 }
 
 // DefaultConfig provides a default configuration to create a new client object
@@ -16,6 +26,9 @@ func DefaultConfig() Config {
 	newConfig := Config{
 		// Dependencies.
 		RestClient: resty.New(),
+
+		// Settings.
+		Address: "http://127.0.0.1:8080",
 	}
 
 	return newConfig
@@ -23,13 +36,36 @@ func DefaultConfig() Config {
 
 // New creates a new configured client object.
 func New(config Config) (*Client, error) {
+	u, err := url.Parse(config.Address)
+	if err != nil {
+		return nil, maskAny(err)
+	}
+
+	creatorConfig := creator.DefaultConfig()
+	creatorConfig.RestClient = config.RestClient
+	creatorConfig.URL = u
+	newCreatorService, err := creator.New(creatorConfig)
+	if err != nil {
+		return nil, maskAny(err)
+	}
+
+	rootConfig := root.DefaultConfig()
+	rootConfig.RestClient = config.RestClient
+	rootConfig.URL = u
+	newRootService, err := root.New(rootConfig)
+	if err != nil {
+		return nil, maskAny(err)
+	}
+
 	newClient := &Client{
-		Config: config,
+		Creator: newCreatorService,
+		Root:    newRootService,
 	}
 
 	return newClient, nil
 }
 
 type Client struct {
-	Config
+	Creator *creator.Service
+	Root    *root.Service
 }
