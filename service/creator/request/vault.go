61a71ec8b0ac2ebdc73f0634c3d6bd84869d833a
Tim Schindler <xh3b4sd@gmail.com>
2017-01-11 18:53:40 +0100

    extended vault config with address

--- service/creator/request/vault.go
+++ service/creator/request/vault.go
@@ -2,12 +2,14 @@ package request
 
 // Vault represents the information necessary to deal with Vault.
 type Vault struct {
-	Token string `json:"token"`
+	Address string `json:"address"`
+	Token   string `json:"token"`
 }
 
 // DefaultVault provides a default Vault configuration by best effort.
 func DefaultVault() Vault {
 	return Vault{
-		Token: "",
+		Address: "",
+		Token:   "",
 	}
 }
