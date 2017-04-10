aee5e116aec29a9310ed7e6e5f4fba5be6143e6a
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-04-10 15:19:18 +0100

    Updates size GB to use int instead of string for storage and memory

--- service/creator/request/storage.go
+++ service/creator/request/storage.go
@@ -2,12 +2,12 @@ package request
 
 // Storage configures the machine storage.
 type Storage struct {
-	SizeGB string `json:"size_gb"`
+	SizeGB int `json:"size_gb"`
 }
 
 // DefaultStorage provides a default storage configuration by best effort.
 func DefaultStorage() Storage {
 	return Storage{
-		SizeGB: "",
+		SizeGB: 0,
 	}
 }
