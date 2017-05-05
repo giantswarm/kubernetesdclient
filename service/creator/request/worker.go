2089bac956ca2a92cc8f02679b5cf33e9922576e
Tim Schindler <xh3b4sd@gmail.com>
2017-05-05 17:30:29 +0200

    added instance type to worker

--- service/creator/request/worker.go
+++ service/creator/request/worker.go
@@ -1,11 +1,16 @@
 package request
 
+import (
+	"github.com/giantswarm/kubernetesdclient/service/creator/request/aws"
+)
+
 // Worker configures the Kubernetes worker nodes.
 type Worker struct {
 	CPU     CPU               `json:"cpu"`
 	Labels  map[string]string `json:"labels"`
 	Memory  Memory            `json:"memory"`
 	Storage Storage           `json:"storage"`
+	AWS     aws.Worker        `json:"aws"`
 }
 
 // DefaultWorker provides a default worker configuration by best effort.
@@ -15,5 +20,6 @@ func DefaultWorker() Worker {
 		Labels:  map[string]string{},
 		Memory:  DefaultMemory(),
 		Storage: DefaultStorage(),
+		AWS:     aws.DefaultWorker(),
 	}
 }
