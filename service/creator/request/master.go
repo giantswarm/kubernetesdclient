53a999173be0ab26764e23f9c4dde07249f5af3f
Tim Schindler <xh3b4sd@gmail.com>
2017-05-05 18:02:21 +0200

    added instance type to master node

--- service/creator/request/master.go
+++ service/creator/request/master.go
@@ -1,10 +1,15 @@
 package request
 
+import (
+	"github.com/giantswarm/kubernetesdclient/service/creator/request/aws"
+)
+
 // Master configures the Kubernetes master nodes.
 type Master struct {
-	CPU     CPU     `json:"cpu"`
-	Memory  Memory  `json:"memory"`
-	Storage Storage `json:"storage"`
+	CPU     CPU        `json:"cpu"`
+	Memory  Memory     `json:"memory"`
+	Storage Storage    `json:"storage"`
+	AWS     aws.Master `json:"aws"`
 }
 
 // DefaultMaster provides a default master configuration by best effort.
@@ -13,5 +18,6 @@ func DefaultMaster() Master {
 		CPU:     DefaultCPU(),
 		Memory:  DefaultMemory(),
 		Storage: DefaultStorage(),
+		AWS:     aws.DefaultMaster(),
 	}
 }
