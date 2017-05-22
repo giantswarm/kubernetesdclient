2a890ec70c71d2207daae9e95746d24efa2951e5
Tim Schindler <xh3b4sd@gmail.com>
2017-05-22 13:13:55 +0200

    added node IDs to master and worker requests (#16)

--- service/creator/request/master.go
+++ service/creator/request/master.go
@@ -6,18 +6,20 @@ import (
 
 // Master configures the Kubernetes master nodes.
 type Master struct {
+	AWS     aws.Master `json:"aws"`
 	CPU     CPU        `json:"cpu"`
+	ID      string     `json:"id"`
 	Memory  Memory     `json:"memory"`
 	Storage Storage    `json:"storage"`
-	AWS     aws.Master `json:"aws"`
 }
 
 // DefaultMaster provides a default master configuration by best effort.
 func DefaultMaster() Master {
 	return Master{
+		AWS:     aws.DefaultMaster(),
 		CPU:     DefaultCPU(),
+		ID:      "",
 		Memory:  DefaultMemory(),
 		Storage: DefaultStorage(),
-		AWS:     aws.DefaultMaster(),
 	}
 }
