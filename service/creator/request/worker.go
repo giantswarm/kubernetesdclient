efee73c2dab336ed16ea7bba6eab0b00af9475fa
Tim Schindler <xh3b4sd@gmail.com>
2017-05-22 13:01:48 +0200

    added node IDs to master and worker requests

--- service/creator/request/worker.go
+++ service/creator/request/worker.go
@@ -6,20 +6,22 @@ import (
 
 // Worker configures the Kubernetes worker nodes.
 type Worker struct {
+	AWS     aws.Worker        `json:"aws"`
 	CPU     CPU               `json:"cpu"`
+	ID      string            `json:"id"`
 	Labels  map[string]string `json:"labels"`
 	Memory  Memory            `json:"memory"`
 	Storage Storage           `json:"storage"`
-	AWS     aws.Worker        `json:"aws"`
 }
 
 // DefaultWorker provides a default worker configuration by best effort.
 func DefaultWorker() Worker {
 	return Worker{
+		AWS:     aws.DefaultWorker(),
 		CPU:     DefaultCPU(),
+		ID:      "",
 		Labels:  map[string]string{},
 		Memory:  DefaultMemory(),
 		Storage: DefaultStorage(),
-		AWS:     aws.DefaultWorker(),
 	}
 }
