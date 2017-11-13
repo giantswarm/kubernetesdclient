f60bbbebab8dfe91a9ceffbefcb2619815a51647
Tim Schindler <xh3b4sd@gmail.com>
2017-11-13 11:58:14 +0100

    added version bundles to creator (#25)

--- service/creator/request.go
+++ service/creator/request.go
@@ -3,36 +3,37 @@ package creator
 import (
 	"time"
 
+	"github.com/giantswarm/versionbundle"
+
 	"github.com/giantswarm/kubernetesdclient/service/creator/request"
 )
 
 // Request is the configuration for the service action.
 type Request struct {
-	APIEndpoint string    `json:"api_endpoint"`
-	CreateDate  time.Time `json:"create_date"`
-	ID          string    `json:"id"`
-	Name        string    `json:"name,omitempty"`
-
-	Owner string `json:"owner,omitempty"`
-
-	KubernetesVersion string `json:"kubernetes_version,omitempty"`
-
-	Masters []request.Master `json:"masters,omitempty"`
-	Vault   request.Vault    `json:"vault,omitempty"`
-	Workers []request.Worker `json:"workers,omitempty"`
+	APIEndpoint       string                 `json:"api_endpoint"`
+	CreateDate        time.Time              `json:"create_date"`
+	ID                string                 `json:"id"`
+	KubernetesVersion string                 `json:"kubernetes_version,omitempty"`
+	Masters           []request.Master       `json:"masters,omitempty"`
+	Name              string                 `json:"name,omitempty"`
+	Owner             string                 `json:"owner,omitempty"`
+	VersionBundles    []versionbundle.Bundle `json:"version_bundles,omitempty"`
+	Vault             request.Vault          `json:"vault,omitempty"`
+	Workers           []request.Worker       `json:"workers,omitempty"`
 }
 
 // DefaultRequest provides a default request object by best effort.
 func DefaultRequest() Request {
 	return Request{
-		Name: "",
-
-		Owner: "",
-
+		APIEndpoint:       "",
+		CreateDate:        time.Time{},
+		ID:                "",
 		KubernetesVersion: "",
-
-		Masters: []request.Master{},
-		Vault:   request.DefaultVault(),
-		Workers: []request.Worker{},
+		Masters:           []request.Master{},
+		Name:              "",
+		Owner:             "",
+		VersionBundles:    []versionbundle.Bundle{},
+		Vault:             request.DefaultVault(),
+		Workers:           []request.Worker{},
 	}
 }
