f9dff46230fcfd52451895a7d364d8e7c7ff38fb
Oliver Ponder <oliver.ponder@gmail.com>
2018-03-05 10:55:30 +0100

    Rename package to request.

--- service/searcher/request.go
+++ service/searcher/request.go
@@ -1,6 +1,6 @@
 package searcher
 
-import "github.com/giantswarm/kubernetesdclient/service/searcher/config"
+import "github.com/giantswarm/kubernetesdclient/service/searcher/request"
 
 // Request is the configuration for the service action.
 type Request struct {
