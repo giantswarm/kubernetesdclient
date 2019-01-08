84be08d717aa5ad29becfc55289c6fa201f90fe2
Tim Schindler <xh3b4sd@gmail.com>
2019-01-08 15:34:41 +0100

    adds scaling to updater request

--- service/updater/config/patch.go
+++ service/updater/config/patch.go
@@ -4,6 +4,7 @@ import "github.com/giantswarm/versionbundle"
 
 // Patch is the cluster specific configuration.
 type Patch struct {
+	Scaling        Scaling                `json:"scaling,omitempty"`
 	VersionBundles []versionbundle.Bundle `json:"version_bundles,omitempty"`
 	Workers        []Worker               `json:"workers,omitempty"`
 }
@@ -11,6 +12,7 @@ type Patch struct {
 // DefaultPatch provides a default patch by best effort.
 func DefaultPatch() Patch {
 	return Patch{
+		Scaling:        Scaling{},
 		VersionBundles: []versionbundle.Bundle{},
 		Workers:        []Worker{},
 	}
