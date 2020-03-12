be444bf4a226d1dce57823c81bd9db2c6cca267a
Zach Stone <zach@giantswarm.io>
2020-03-12 10:04:01 +0100

    Add ReleaseVersion to Patch (#46)

--- service/updater/config/patch.go
+++ service/updater/config/patch.go
@@ -4,6 +4,7 @@ import "github.com/giantswarm/versionbundle"
 
 // Patch is the cluster specific configuration.
 type Patch struct {
+	ReleaseVersion string                 `json:"release_version,omitempty"`
 	Scaling        Scaling                `json:"scaling,omitempty"`
 	VersionBundles []versionbundle.Bundle `json:"version_bundles,omitempty"`
 	Workers        []Worker               `json:"workers,omitempty"`
@@ -12,6 +13,7 @@ type Patch struct {
 // DefaultPatch provides a default patch by best effort.
 func DefaultPatch() Patch {
 	return Patch{
+		ReleaseVersion: "",
 		Scaling:        Scaling{},
 		VersionBundles: []versionbundle.Bundle{},
 		Workers:        []Worker{},
